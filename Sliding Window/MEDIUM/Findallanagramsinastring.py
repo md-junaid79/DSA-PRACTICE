3fe5d0f418a7a5842191627b6142218f287ca5e9
JUNAID <moinuddinm942@gmail.com>
2025-07-29 23:33:10 +0530

    updated findAnagrams

--- Sliding Window/MEDIUM/Findallanagramsinastring.py
+++ Sliding Window/MEDIUM/Findallanagramsinastring.py
@@ -5,3 +5,26 @@ Explanation: The substring with start index = 0 is "cba", which is an anagram of
 
 Example 2: Input: s = "abab", p = "ab" Output: [0,1,2]
 '''
+from collections import Counter               #if counter is not allowed then we can use freq arr (dictionary) and ord(i) etc..
+def findAnagrams (s,p): 
+    len_s,len_p=len(s),len(p)
+    res=[]
+    if len_s<len_p:
+        return res
+    p_count=Counter(p)  # Count of characters in p .it returns a dictionary
+    window_count=Counter(s[:len_p])  # Count of characters in the first window
+    
+    if window_count == p_count:
+        res.append(0)
+
+        for i in range(len_p,len_s):     #len_p to len_s because we increment right pointer 
+        window_count[s[i]] += 1  # Add the new character to the window
+        # left pointer is at i - len_p, removing it
+        window_count[s[i - len_p]] -= 1  # Remove the character going out 
+        
+        if window_count[s[i-len_p]] == 0:
+            del window_count[s[i - len_p]]  #deleting so that next step-> is not affected
+        if window_count == p_count:
+            res.append(i-len_p + 1)  
+    return res
+      
